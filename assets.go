f7843e2a3cd1c5289e11d46fdbdd192aba8c043f
Viktor Persson <viktor.persson@arcsin.se>
2022-09-16 18:36:16 +0200

    Add DownloadMap(), Download() to asset manager.

--- assets.go
+++ assets.go
@@ -3,17 +3,21 @@ package ezquake
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/cavaliergopher/grab/v3"
 )
 
 type AssetManager struct {
-	baseDirPath string
+	baseDir string
 }
 
-func NewAssetManager(baseDirpath string) *AssetManager {
+func NewAssetManager(baseDirAbsPath string) *AssetManager {
 	return &AssetManager{
-		baseDirPath: baseDirpath,
+		baseDir: baseDirAbsPath,
 	}
 }
 
@@ -23,11 +27,29 @@ func (a *AssetManager) AbsPath(path ...string) string {
 }
 
 func (a *AssetManager) BaseDir() string {
-	return a.baseDirPath
+	return a.baseDir
 }
 
 func (a *AssetManager) DemosDir() string {
 	return a.AbsPath(filepath.Join("qw", "demos"))
+}
+
+func (a *AssetManager) DownloadMap(name string) error {
+	if a.HasMap(name) {
+		return nil
+	}
+
+	return a.Download(MapUrl(name), a.MapsDir())
+}
+
+func (a *AssetManager) Download(url string, destDir string) error {
+	resp, err := grab.Get(destDir, url)
+
+	if http.StatusNotFound == resp.HTTPResponse.StatusCode {
+		return errors.New(fmt.Sprintf("NOT FOUND: %s", url))
+	}
+
+	return err
 }
 
 func (a *AssetManager) HasFileOrDir(path ...string) bool {
@@ -43,9 +65,17 @@ func (a *AssetManager) HasMap(name string) bool {
 }
 
 func (a *AssetManager) MapPath(name string) string {
-	return filepath.Join(a.MapsDir(), fmt.Sprintf("%s.bsp", name))
+	return filepath.Join(a.MapsDir(), MapFilename(name))
 }
 
 func (a *AssetManager) MapsDir() string {
 	return a.AbsPath(filepath.Join("qw", "maps"))
+}
+
+func MapFilename(name string) string {
+	return fmt.Sprintf("%s.bsp", strings.TrimSuffix(name, ".bsp"))
+}
+
+func MapUrl(name string) string {
+	return fmt.Sprintf("https://maps.quakeworld.nu/all/%s", MapFilename(name))
 }
--- assets_test.go
+++ assets_test.go
@@ -1,6 +1,7 @@
 package ezquake_test
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -11,18 +12,6 @@ import (
 func GetAssetManager() *ezquake.AssetManager {
 	quakeDirAbsPath, _ := filepath.Abs("./test_files/quake")
 	return ezquake.NewAssetManager(quakeDirAbsPath)
-}
-
-func TestAssetManager_HasMap(t *testing.T) {
-	manager := GetAssetManager()
-	assert.True(t, manager.HasMap("foo"))
-	assert.False(t, manager.HasMap("dm6"))
-}
-
-func TestAssetManager_MapPath(t *testing.T) {
-	manager := GetAssetManager()
-	expect, _ := filepath.Abs("./test_files/quake/qw/maps/dm6.bsp")
-	assert.Equal(t, expect, manager.MapPath("dm6"))
 }
 
 func TestAssetManager_AbsPath(t *testing.T) {
@@ -43,8 +32,58 @@ func TestAssetManager_DemosDir(t *testing.T) {
 	assert.Equal(t, expect, manager.DemosDir())
 }
 
+func TestAssetManager_DownloadMap(t *testing.T) {
+	manager := GetAssetManager()
+
+	t.Run("map not found on maps.quakeworld.nu", func(t *testing.T) {
+		err := manager.DownloadMap("foobar")
+		mapAbsPath, _ := filepath.Abs("./test_files/quake/qw/maps/foobar.bsp")
+		assert.NoFileExists(t, mapAbsPath)
+		assert.ErrorContains(t, err, "NOT FOUND: https://maps.quakeworld.nu/all/foobar.bsp")
+	})
+
+	t.Run("map already exist in qw/maps", func(t *testing.T) {
+		err := manager.DownloadMap("dm2")
+		mapAbsPath, _ := filepath.Abs("./test_files/quake/qw/maps/dm2.bsp")
+		assert.FileExists(t, mapAbsPath)
+		assert.Nil(t, err)
+	})
+
+	t.Run("new map", func(t *testing.T) {
+		err := manager.DownloadMap("fragtwn1")
+		mapAbsPath, _ := filepath.Abs("./test_files/quake/qw/maps/fragtwn1.bsp")
+		assert.FileExists(t, mapAbsPath)
+		assert.Nil(t, err)
+
+		os.Remove(manager.MapPath("fragtwn1"))
+	})
+}
+
+func TestAssetManager_HasMap(t *testing.T) {
+	manager := GetAssetManager()
+	assert.True(t, manager.HasMap("dm2"))
+	assert.True(t, manager.HasMap("dm2.bsp"))
+	assert.False(t, manager.HasMap("dm6"))
+}
+
+func TestAssetManager_MapPath(t *testing.T) {
+	manager := GetAssetManager()
+	expect, _ := filepath.Abs("./test_files/quake/qw/maps/dm6.bsp")
+	assert.Equal(t, expect, manager.MapPath("dm6"))
+}
+
 func TestAssetManager_MapsDir(t *testing.T) {
 	manager := GetAssetManager()
 	expect, _ := filepath.Abs("./test_files/quake/qw/maps")
 	assert.Equal(t, expect, manager.MapsDir())
+}
+
+func TestMapFilename(t *testing.T) {
+	assert.Equal(t, "dm6.bsp", ezquake.MapFilename("dm6"))
+	assert.Equal(t, "dm6.bsp", ezquake.MapFilename("dm6.bsp"))
+}
+
+func TestMapUrl(t *testing.T) {
+	assert.Equal(t, "https://maps.quakeworld.nu/all/dm6.bsp", ezquake.MapUrl("dm6"))
+	assert.Equal(t, "https://maps.quakeworld.nu/all/dm6.bsp", ezquake.MapUrl("dm6.bsp"))
 }
